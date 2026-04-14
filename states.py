4dbb8709a8729552827c7961aa7bd57ce712720f
Андрей Дашков <adashkovv@gmail.com>
2026-04-14 10:51:07 +0300

    states.py

--- states.py
+++ states.py
@@ -5,6 +5,5 @@ class OrderStates(StatesGroup):
     entering_deadline = State()
     choosing_execution = State()
     choosing_extras = State()
-    uploading_photos = State()
-    entering_texts = State()
-    confirming = State()
+    confirming = State()        # подтверждение заказа
+    waiting_for_contact = State()  # новый этап: написать девушке
